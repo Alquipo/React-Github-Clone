14b76e360b42d22d6384039fbfb6e36d9b91f82a
Alquipo <alquiponeto@outlook.com.br>
2020-08-12 16:53:11 -0300

    feat: routes App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import GlobalStyles from "./styles/GlobalStyles";
+import Header from "./components/Header";
+import Profile from "./pages/Profile";
+import Repo from "./pages/Repo";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Profile />} />
+        <Route path="/:username" element={<Profile />} />
+        <Route path="/:username/:reponame" element={<Repo />} />
+      </Routes>
+
+      {/* <Footer /> */}
+
+      <GlobalStyles />
+    </BrowserRouter>
+  );
 }
 
 export default App;
